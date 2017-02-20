9fcf69da52d1a8b008aad7074a3b7bb59f295193
Philippe Marschall <philippe.marschall@gmail.com>
2017-02-20 22:23:42 +0100

    - add INTERVALDS type - add INTERVALYM type

--- threeten-jpa-oracle-api/src/main/java/oracle/jdbc/OracleTypes.java
+++ threeten-jpa-oracle-api/src/main/java/oracle/jdbc/OracleTypes.java
@@ -1,7 +1,14 @@
 package oracle.jdbc;
 
+/**
+ * <a href="https://docs.oracle.com/database/121/JAJDB/oracle/jdbc/OracleTypes.html">OracleTypes</a>
+ */
 public class OracleTypes {
 
   public static final int TIMESTAMPTZ = -101;
+
+  public static final int INTERVALDS = -104;
+
+  public static final int INTERVALYM = -103;
 
 }
