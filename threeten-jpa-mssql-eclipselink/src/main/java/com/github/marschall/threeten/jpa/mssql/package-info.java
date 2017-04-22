3ce037640531c48524b24091c8f167a83b0d95cb
Philippe Marschall <philippe.marschall@gmail.com>
2017-04-22 21:12:22 +0200

    add package info

---
+++ threeten-jpa-mssql-eclipselink/src/main/java/com/github/marschall/threeten/jpa/mssql/package-info.java
@@ -0,0 +1,33 @@
+/**
+ * Contains additional classed for SQL Server and EclipseLink.
+ *
+ * <h3>Attribute Converters</h3>
+ * Contains additional attribute converters for SQL Server that currently only
+ * work with EclipseLink.
+ * <p>
+ * All the converters have set
+ * {@link javax.persistence.Converter#autoApply()} to {@code true} to
+ * they're automatically applied to all entities in the same persistence
+ * unit.
+ * <p>
+ * The H2 driver module has to be visible to the deployment
+ * (eg <a href="https://docs.jboss.org/author/display/WFLY10/Class+Loading+in+WildFly">Class Loading in WildFly</a>).
+ * <table>
+ *  <caption>Supported type conversions</caption>
+ *  <thead>
+ *    <tr>
+ *      <td>Transact-SQL</td>
+ *      <td>Java SE 8</td>
+ *      <td>Converter</td>
+ *     </tr>
+ *  </thead>
+ *  <tbody>
+ *    <tr>
+ *      <td><code>DATETIMEOFFSET</code></td>
+ *      <td>{@link java.time.OffsetDateTime}</td>
+ *      <td>{@link com.github.marschall.threeten.jpa.mssql.MssqlOffsetDateTimeConverter}</td>
+ *    </tr>
+ *  </tbody>
+ * </table>
+ */
+package com.github.marschall.threeten.jpa.mssql;
