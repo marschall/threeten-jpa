e7a61e7a810beb52a0160bc882973f3b5c9151d5
Philippe Marschall <philippe.marschall@gmail.com>
2017-06-07 11:14:22 +0200

    enable

--- threeten-jpa-jdbc42-hibernate/src/test/java/com/github/marschall/threeten/jpa/jdbc42/hibernate/HsqlTest.java
+++ threeten-jpa-jdbc42-hibernate/src/test/java/com/github/marschall/threeten/jpa/jdbc42/hibernate/HsqlTest.java
@@ -12,7 +12,6 @@ import java.util.UUID;
 
 import javax.sql.DataSource;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
