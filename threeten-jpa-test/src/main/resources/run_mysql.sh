1d69218613b74359aff46731a94156cf2d15c3f1
Philippe Marschall <philippe.marschall@gmail.com>
2020-04-13 10:56:42 +0200

    bump mysql

--- threeten-jpa-test/src/main/resources/run_mysql.sh
+++ threeten-jpa-test/src/main/resources/run_mysql.sh
@@ -6,5 +6,5 @@ docker run --name jdbc-mysql \
  -e MYSQL_DATABASE=$USER \
  -p 3306:3306 \
  --mount type=tmpfs,destination=/var/lib/mysql \
- -d mysql:8.0.15 \
+ -d mysql:8.0.19 \
  --log-bin-trust-function-creators=1
